a333d3c9cc14aca7c1f98f84268d0ef834c00c39
caleb <odedejikehinde08@gmail.com>
2023-07-24 16:33:06 -0700

    chnages made

--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 const express = require("express");
 const app = express();
 
+app.post("/", (req, res) => {
+  // const username = req.query.username || "myogeshchavan97";
+  try {
+    res.status(200).send("Hello World,First Deployment");
+  } catch (error) {
+    res.status(400).send("Error while getting list of repositories");
+  }
+});
+
 // creating server PORT
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
