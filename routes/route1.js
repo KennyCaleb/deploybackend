d948dfe01866440e4665c27b096fbca98a14538f
caleb <odedejikehinde08@gmail.com>
2023-07-24 17:59:03 -0700

    chnages made

--- routes/route1.js
+++ routes/route1.js
@@ -3,6 +3,13 @@ const router = express.Router()
 
 router.get("/", (req, res)=>{
     res.status(200).send("Router in operation.")
+    res.status(200).json({msg:"JSON Format."})
+})
+
+router.post("/add", (req, res)=>{
+    const user = req.body
+
+    res.status(200).send(user)
 })
 
 module.exports = router
