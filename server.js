43e289a63d94f5b8f033fb2f7e34ec43acd35c0a
caleb <odedejikehinde08@gmail.com>
2023-07-24 17:29:59 -0700

    chnages made

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // importing modules
 const express = require("express");
 const app = express();
+const router1 = require("./routes/route1")
 
-app.get("/", (req, res) => {
-  // const username = req.query.username || "myogeshchavan97";
-  try {
-    res.status(200).send("Hello World,First Deployment");
-  } catch (error) {
-    res.status(400).send("Error while getting list of repositories");
-  }
-});
+// app.get("/", (req, res) => {
+//   // const username = req.query.username || "myogeshchavan97";
+//   try {
+//     res.status(200).send("Hello World,First Deployment");
+//   } catch (error) {
+//     res.status(400).send("Error while getting list of repositories");
+//   }
+// });
+
+// middlewares
+app.use("/", router1)
 
 // creating server PORT
 let PORT = process.env.PORT || 5000;
